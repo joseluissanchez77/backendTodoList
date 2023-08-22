e01762a135188dc83325e3b40403c50258e8642f
joseluissanchez77 <joseluissanchez41@hotmail.com>
2023-08-22 06:45:41 -0500

    lista de usuarios

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('user/', views.get_user_data),
     path('register/', views.register_api),
     path('logout/', knox_views.LogoutView.as_view()),
-    path('logoutall/', knox_views.LogoutAllView.as_view())
+    path('logoutall/', knox_views.LogoutAllView.as_view()),
+    path('list-user/', views.get_list_users),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.auth import AuthToken
 from .serializers import  RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
 
 @api_view(['POST'])
 def login_api(request):
@@ -51,3 +53,18 @@ def register_api(request):
         },
         'token':token
         })
+
+
+
+@api_view(['GET'])
+def get_list_users(request):
+    user = request.user
+
+    if user.is_authenticated == False:
+        return Response({'error': 'No Autenticado'}, status.HTTP_401_UNAUTHORIZED)
+    
+    snippets = User.objects.all()
+    serializer = RegisterSerializer(snippets, many=True)
+    return Response(serializer.data, status.HTTP_200_OK)
+
+# https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
